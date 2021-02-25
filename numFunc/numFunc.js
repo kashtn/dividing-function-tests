b3101be1e1c6026c22956727bb22981ddbf8cadc
IvanMusk <ivan070797@yandex.ru>
2021-02-25 22:07:43 +0300

    tests are ready

---
+++ numFunc/numFunc.js
@@ -0,0 +1,12 @@
+/*
+
+a, b = Number
+
+return Number
+
+*/
+
+module.exports = async (a, b) => {
+  console.log('',a / b);
+  return a / b;
+};
---
+++ numFunc/numFunc.test.js
@@ -0,0 +1,22 @@
+const func = require("./numFunc");
+
+describe("Testing the dividing function", () => {
+  const mockCallback = jest.fn(func);
+
+  test("Arguments should be numbers", () => {
+    return mockCallback(4, 2).then((result) => {
+      expect(typeof mockCallback.mock.calls[0][0]).toBe("number");
+      expect(typeof mockCallback.mock.calls[0][1]).toBe("number");
+    });
+  });
+
+  test("Should return only a number", () => {
+    return mockCallback(0.001, 200).then((result) => {
+      expect(result).toBe(0.000005)
+      expect(typeof result).toBe("number");
+      expect(result).not.toBe(NaN);
+      expect(result).toBeGreaterThanOrEqual(0);
+      expect(result).not.toBe(Infinity);
+    });
+  });
+});
